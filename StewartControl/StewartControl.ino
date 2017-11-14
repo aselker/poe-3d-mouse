fd6b7e42c9cf4bcbce9fe0b6d55392d3c4c4fd68
Adam Selker <adamselker@gmail.com>
2017-11-14 11:56:35 -0500

    No longer a dumbass.

--- StewartControl/StewartControl.ino
+++ StewartControl/StewartControl.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 Servo servos[6];
-const int[] servoPins = {11, 10, 9, 6, 5, 3};
+const int servoPins[] = {11, 10, 9, 6, 5, 3};
 float lowerLimit = -30; //The lowest position they're allowed to go to
 
 int pos = 0;    // variable to store the servo position
@@ -16,28 +16,22 @@ void setup() {
 
 void loop() {
 
-  if (Serial.available()) {
-    int i = 0;
+  if (Serial.available()) for (int i = 0; i < 6; i++) {
     String input = "";
     while (Serial.available()) {
       input += Serial.read();
-      if (input[input.length()-1] == ',') { //If we've read until a comma
-
-    for (int i = 0; i < 6; i++) {
-      while (Serial.available()) {
-        input += Serial.read();
-        if (input[input.length()-1] == ','|| input[input.length()-1] == "\n") break; //Keep reading till we hit a comma, or the EOL
-      }
-      if (atoi(input.c_str) > lowerLimit) { //Limit the range
-        if (i % 2 == 0) servoPos[i] = (90.0 + atoi(input.c_str())) * 255.0 / 180.0; //Convert from centered degrees to zero-based PWM values
-        else servoPos[i] = (90.0 - atoi(input.c_str())) * 255.0 / 180.0;
-      } else {
-        if (i % 2 == 0) servoPos[i] = (90.0 + lowerLimit) * 255.0 / 180.0; //Convert from centered degrees to zero-based PWM values
-        else servoPos[i] = (90.0 - lowerLimit) * 255.0 / 180.0;
-      }
+      if (input[input.length()-1] == ','|| input[input.length()-1] == "\n") break; //Keep reading till we hit a comma, or the EOL
     }
+    if (atoi(input.c_str()) > lowerLimit) { //Limit the range
+      if (i % 2 == 0) servoPos[i] = (90.0 + atoi(input.c_str())) * 255.0 / 180.0; //Convert from centered degrees to zero-based PWM values
+      else servoPos[i] = (90.0 - atoi(input.c_str())) * 255.0 / 180.0;
+    } else {
+      if (i % 2 == 0) servoPos[i] = (90.0 + lowerLimit) * 255.0 / 180.0; //Convert from centered degrees to zero-based PWM values
+      else servoPos[i] = (90.0 - lowerLimit) * 255.0 / 180.0;
+    }
+  }
 
-    for (int i = 0; i < 6; i++) servos[i].write(servoPos[i]);
+  for (int i = 0; i < 6; i++) servos[i].write(servoPos[i]);
  
 }
 
