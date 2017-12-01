9f77defe7a3f1c722a5a8b1e72ae130cdf381a85
Adam Selker <adamselker@gmail.com>
2017-11-30 19:10:10 -0500

    Made code to test soft control of six servos.

---
+++ SixSoftServos/SixSoftServos.ino
@@ -0,0 +1,34 @@
+#include "softServo.h"
+
+const int enablePins[] = {3,4,5,6,9,10};
+const int aPins[] = {2,7,11,33,31,28};
+const int bPins[] = {13,8,12,32,30,27};
+const int potPins[] = {A0, A2, A4, A6, A8, A10};
+
+const float kp = 10, ki= 7, kd = 2;
+
+softServo servos[6];
+
+const int center = 512, motionRange = 100; //Out of 1024
+const float speed = 0.2; //Roughly(?) how many cycles per second to move
+const float offsets[] = {0, PI/3., 2.*PI/3., PI, 4.*PI/3., 5.*PI/3.};
+
+void setup() {
+
+  for(int i = 0; i < 6; i++) {
+    servos[i].setup(enablePins[i], aPins[i], bPins[i], potPins[i], kp, ki, kd);
+  }
+
+}
+
+void loop() {
+
+  for(int i = 0; i < 6; i++) {
+    int phase = sin(float(millis())/1000*speed*2*PI + offsets[i]);
+    servos[i].setPos(center + (phase * motionRange/2));
+    servos[i].update();
+  }
+  
+  delay(10);
+
+}
---
+++ SixSoftServos/softServo.h
@@ -0,0 +1,46 @@
+// Library for working with gutted hobby servos
+
+#ifndef SOFTSERVO_H
+#define SOFTSERVO_H
+#include "Arduino.h"
+
+//using namespace std;
+
+class softServo {
+
+  public:
+    softServo(); //Default constructor, for making lists
+    softServo(int enablePin, int aPin, int bPin, int potPin, float kp, float ki, float kd);
+
+    void setup(int enablePin, int aPin, int bPin, int potPin, float kp, float ki, float kd);
+    //There are better ways to move these vars -- initializer lists and tuples -- but they require c++11
+
+    void update(); //Actually reads the pot / sets the motor power; call regularly for best results
+
+    void setPower(int power); //Set motor power, ignore pot
+
+    void setPids(float kp, float ki, float kd); //Reset the pid tuning
+    void setPos(int pos); //Go to a position, using current pids. Position is in whatever unit the Arduino uses for analog sensing (usually 0-1023 for full range).
+    int getPos(); //Where is it *actually*?
+
+  private:
+
+    static const int winLen = 1024, maxPower = 96; //Running at 9v for now -- don't go above around 128
+
+    bool pinsInitialized;
+    int enablePin, aPin, bPin, potPin;
+
+    bool isPos; //True when we're trying to get to a position
+    int power; //How hard we're pushing right now -- reset every update if isPos
+    int goalPos; //If isPos, try to go here
+
+    float kp, ki, kd; //Gains
+
+    int window[winLen], lastError, winPos, total, pos; //Some state
+
+    long unsigned int lastTime, loopTime; //The time the last loop finished, and how long it took
+
+
+};
+
+#endif
