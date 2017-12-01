b46ce0bfd5d305bdfefa8b86033767d9ac8e695c
Harrison Young <hyoung@olin.edu>
2017-12-01 03:53:45 -0500

    added serial comunication

--- PoeGame2/Assets/Scripts/PlayerController.cs
+++ PoeGame2/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+using System.IO.Ports;
 public class PlayerController : MonoBehaviour
 {
  
     public float speed;
     private Rigidbody player;
     public string data = "";
-     
-           
+    public string stewartPosition = "";
+    public Transform target;
+    SerialPort serial = new SerialPort("COM6", 115200);
+    public float[] stewartInput = new float[6];
+ 
+
     // Use this for initialization
     void Start()
     {
@@ -19,14 +23,36 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
 
+        //movingData(stewartInput);
         moving();
-        rotating();
+       // rotating(stewartInput);
         output();
-        breakjoint();       
+        breakjoint();
+        writeToArduino();
+
+    }
+    void Update()
+    {
+        if (!serial.IsOpen)
+        {
+            serial.Open();
+
+        }
+        if (serial.IsOpen)
+        {
+              stewartPosition = readarduino(); // reads poistion and angles from arduino
+            stewartInput = readStewartPosition(stewartPosition);
+        }
+         serial.ReadTimeout = 500;
+         serial.WriteTimeout = 500;
 
 
     }
-   
+    void LateUpdate()
+    {
+        limitAngle();
+    }
+
     void OnCollisionStay(Collision other)
     {
         
@@ -46,20 +72,21 @@ public class PlayerController : MonoBehaviour
         float z =player.transform.rotation[1];
         float x = player.transform.rotation[2];
         float y = player.transform.rotation[0];
-               float vary = (Mathf.Clamp(y, -0.20f,0.2f));
-        float varz = (Mathf.Clamp(z, -0.2f, 0.2f));
-        float varx = (Mathf.Clamp(x, -0.2f, 0.2f));
+               float vary = (Mathf.Clamp(y, -0.40f,0.4f));
+        float varz = (Mathf.Clamp(z, -0.4f, 0.4f));
+        float varx = (Mathf.Clamp(x, -0.4f, 0.4f));
         data = player.transform.rotation[0].ToString("F4")+","+player.transform.rotation[1].ToString("F4")+","+player.transform.rotation[2].ToString("F4")+","+player.transform.rotation[3].ToString("F4");
         print(data);
         player.transform.rotation = new Quaternion(vary, varz, varx, player.transform.rotation[3]);
         
 
     }
-    void rotating()
+    void rotating(float[] moredata)
     {
-        float targetx = 0.0f;
-        float targety = 0.0f;
-        float targetz = 0.01f;
+        Quaternion stewartAngles = Quaternion.Euler(moredata[3], moredata[4], moredata[5]);
+        float targetx = stewartAngles[2];
+        float targety = stewartAngles[0];
+        float targetz = stewartAngles[1];
 
         float z = player.transform.rotation[1];
         float x = player.transform.rotation[2];
@@ -71,6 +98,24 @@ public class PlayerController : MonoBehaviour
 
 
         player.GetComponent<Rigidbody>().AddTorque(torquey, torquez, torquex);
+    }
+    void movingData(float[] moredata)
+    {
+        Vector3 stewartpos = new Vector3(moredata[0], moredata[1], moredata[2]);
+        float targetx = stewartpos[2];
+        float targety = stewartpos[0];
+        float targetz = stewartpos[1];
+
+        float z = player.transform.position[1];
+        float x = player.transform.position[2];
+        float y = player.transform.position[0];
+
+        float forcex = (targetx - x) * 50;
+        float forcez = (targetz - z) * 50;
+        float forcey = (targety - y) * 50;
+
+
+        player.GetComponent<Rigidbody>().AddForce(forcey, forcez, forcex);
     }
     void moving()
     {
@@ -96,7 +141,7 @@ public class PlayerController : MonoBehaviour
         Vector3 bar = transform.position;
         Vector3 bar2 = transform.eulerAngles;
         data = bar[0].ToString("F2") + "," + bar[1].ToString("F2") + "," + bar[2].ToString("F2") + "," + bar2[0].ToString("F0") + "," + bar[1].ToString("F0") + "," + bar[2].ToString("F0") + "," + Time.fixedDeltaTime + "\r\n";
-        System.IO.File.AppendAllText(@"C:\Users\hyoung\Documents\_POE\POE Project\PoeGame2\data.txt", data);
+        //System.IO.File.AppendAllText(@"C:\Users\hyoung\Documents\_POE\POE Project\PoeGame2\data.txt", data);
 
         Debug.Log(data);
     }
@@ -110,7 +155,42 @@ public class PlayerController : MonoBehaviour
 
         }
     }
-   
+    string readarduino()
+    {
+        string arduinoData = "not working";
+        arduinoData = (string)(serial.ReadLine());
+        return arduinoData;
+    }
+
+    float[]  readStewartPosition(string input)
+    {
+        
+        float[] stewartData = new float[6];
+        string[] temp;
+
+        temp = input.Split(',');
+        for( int i = 0; i < 6; i++)
+        {
+            stewartData[i] = float.Parse(temp[i]);
+        }
+        print(stewartData);
+  //      Vector3 stewartLocation = new Vector3(stewartData[0], stewartData[1], stewartData[2]);
+ //       Quaternion stewartAngles = Quaternion.Euler(stewartData[3], stewartData[4], stewartData[5]);
+        
+        return stewartData;
+
+
+    }
+    void writeToArduino()
+    {
+        Vector3 bar = player.transform.position;
+        Vector3 bar2 = player.transform.eulerAngles;
+
+       string outPutData = bar[0].ToString("F2") + "," + bar[1].ToString("F2") + "," + bar[2].ToString("F2") + "," + bar2[0].ToString("F0") + "," + bar[1].ToString("F0") + "," + bar[2].ToString("F0")  + "\r\n";
+        serial.WriteLine(outPutData);
+    }
+
+
 }
 
 	
