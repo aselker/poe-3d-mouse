4945303a016667566a6388429b8fd50b6cced435
Harrison Young <hyoung@olin.edu>
2017-12-04 17:53:30 -0500

    Messed with motor and pot directions

--- SixSoftServos/SixSoftServos.ino
+++ SixSoftServos/SixSoftServos.ino
@@ -2,34 +2,59 @@
 #include "Arduino.h"
 
 
-const int enablePins[] = {3,4,5,6,9,10};
-const int aPins[] = {2,7,11,33,31,28};
-const int bPins[] = {13,8,12,32,30,27};
+const int enablePins[] = {3,5,4,6,9,10};
+const int aPins[] = {2,11,7,33,31,28};
+const int bPins[] = {13,12,8,32,30,27};
 const int potPins[] = {A0, A2, A4, A6, A8, A10};
 
 const float kp = 10, ki= 7, kd = 2;
 
 softServo servos[6];
 
-const int center = 512, motionRange = 100; //Out of 1024
+const int center = 512, motionRange = 0; //Out of 1024
 const float speed = 0.2; //Roughly(?) how many cycles per second to move
 const float offsets[] = {0, PI/3., 2.*PI/3., PI, 4.*PI/3., 5.*PI/3.};
 
 void setup() {
 
+  Serial.begin(9600);
+
   for(int i = 0; i < 6; i++) {
     servos[i].setup(enablePins[i], aPins[i], bPins[i], potPins[i], kp, ki, kd, false, false); //None are reversed, for testing purposes
   }
 
+  servos[0].potReversed = false;
+  servos[0].posReversed = false;
+
+  servos[1].potReversed = true;
+  servos[1].posReversed = true;
+
+  servos[2].potReversed = false;
+  servos[2].posReversed = false;
+
+  servos[3].potReversed = false;
+  servos[3].posReversed = false;
+  
+  servos[4].potReversed = true;
+  servos[4].posReversed = true;
+  
+  servos[5].potReversed = true;
+  servos[5].posReversed = false;
+
 }
 
 void loop() {
-
-  for(int i = 0; i < 6; i++) {
-    int phase = sin(float(millis())/1000*speed*2*PI + offsets[i]);
-    servos[i].setPos(center + (phase * motionRange/2));
+  
+  for(int i = 0; i < 1; i++) {
+    int phase = sin(float(millis())/1000*speed*2*PI);
+    int angle = center + (phase*motionRange);
+    servos[i].setPos(angle);
+    //servos[i].setPower(255); //Actually sets it to maxPower
     servos[i].update();
+    /*Serial.print(angle); Serial.print("\t"); */Serial.print(servos[i].getPos());  Serial.print("\t");
   }
+  Serial.println("");
+  
   
   delay(10);
 
--- SixSoftServos/softServo.cpp
+++ SixSoftServos/softServo.cpp
@@ -1,1 +1,117 @@
-../StewartPlatform/softServo.cpp
+#include "softServo.h"
+
+softServo::softServo() {
+
+  this->pinsInitialized = false;
+
+}
+
+softServo::softServo(int enablePin, int aPin, int bPin, int potPin, float kp, float ki, float kd, bool posReversed, bool potReversed) {
+
+  setup(enablePin, aPin, bPin, potPin, kp, ki, kd, posReversed, potReversed);
+
+}
+
+void softServo::setup(int enablePin, int aPin, int bPin, int potPin, float kp, float ki, float kd, bool posReversed, bool potReversed) {
+
+  this->enablePin = enablePin;
+  this->aPin = aPin;
+  this->bPin = bPin;
+  this->potPin = potPin;
+  this->posReversed = posReversed;
+  this->potReversed = potReversed;
+
+  pinMode(enablePin, OUTPUT);
+  pinMode(aPin, OUTPUT);
+  pinMode(bPin, OUTPUT);
+  pinMode(potPin, INPUT);
+
+  this->pinsInitialized = true;
+
+  setPids(kp, ki, kd); //Also clears I buffer
+
+  setPower(0); 
+
+}
+
+void softServo::setPids(float kp, float ki, float kd) { //Reset the pid tuning
+
+  this->kp = kp;
+  this->ki = ki;
+  this->kd = kd;
+
+  for (int i = 0; i < winLen; i++) window[i] = 0; //re-init integral
+  winPos = 0;
+  total = 0;
+}
+
+void softServo::setPower(int power) {
+
+  this->power = power;
+  isPos = false;
+
+}
+
+void softServo::setPos(int pos) {
+
+
+  if (!isPos) { //If switching from power to position, re-init integral
+    for (int i = 0; i < winLen; i++) window[i] = 0;
+    winPos = 0;
+    total = 0;
+  }
+
+  goalPos = (posReversed ? posRange-pos : pos);
+  isPos = true;
+
+}
+
+int softServo::getPos() {
+
+  return pos;
+
+}
+
+void softServo::update() {
+
+  pos = potReversed ? (posRange-analogRead(potPin)) : analogRead(potPin);
+
+  if (isPos) {
+
+    int error = pos - goalPos;
+
+    winPos++; //Go to the next position in the buffer
+    winPos = winPos % winLen;
+    total += error; //Add the value being added
+    total -= window[winPos]; //Remove the value being removed
+    window[winPos] = error; //Insert the new value, knocking out an older one
+
+    float avg = total / winLen;
+
+    int p = error * kp;
+    int i = avg * ki;
+    int d = float(error - lastError) / float(loopTime) * kd;
+
+    //Serial.println("P: " + String(p) + " I: " + String(i) + "D: " + String(d) );
+    //Serial.println("Last error: " + String(lastError) + " Current error: " + String(error) + " Last loop time: " + String(loopTime));
+
+    power = (p + i + d) * (255.0/float(posRange));
+
+    lastError = error;
+
+  } else {//End if isPos
+    if (posReversed) power = -power; //If setting power, not position, reverse that too
+  }
+
+  if (power > maxPower) power = maxPower;
+  if (power < -maxPower) power = -maxPower;
+
+  analogWrite(enablePin, abs(power));
+  digitalWrite(aPin, (power>0) ? HIGH : LOW);
+  digitalWrite(bPin, (power>0) ? LOW : HIGH);
+
+  loopTime = millis() - lastTime; //For decrementing turn timer
+  lastTime = millis(); //So we'll know how long the next loop takes
+
+
+}
